60f27b1070b21752fc78026fe289e39e9fdf2c2b
newu2012 <newu2011@gmail.com>
2021-10-04 03:23:27 +0500

    init: init commit

---
+++ lib/BusinessCard/Presentation/business_card_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class BusinessCardWidget extends StatelessWidget {
+  const BusinessCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.account_circle,
+                size: 50,
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Flutter McFlutter',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                const Text(
+                  'Experienced App Developer',
+                )
+              ],
+            ),
+          ]),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text('123 Main Street'),
+              Text('(415) 555-0198'),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Icon(Icons.accessibility),
+              Icon(Icons.timer),
+              Icon(Icons.phone_android),
+              Icon(Icons.phone_iphone),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:test_test_test_test/BusinessCard/Presentation/business_card_widget.dart';
+import 'package:test_test_test_test/Camp/Presentation/camp_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const apps = {
+    App('Camp', CampWidget()),
+    App('Business Card', BusinessCardWidget()),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        //  Choose your app there by selecting app by it's title
+        app: apps.firstWhere((element) => element.appTitle == 'Camp'),
+      ),
+    );
+  }
+}
+
+class App {
+  final String appTitle;
+  final Widget appWidget;
+
+  const App(this.appTitle, this.appWidget);
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+    required this.app,
+  }) : super(key: key);
+
+  final App app;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.app.appTitle),
+      ),
+      body: Center(
+        child: widget.app.appWidget,
+      ),
+    );
+  }
+}
