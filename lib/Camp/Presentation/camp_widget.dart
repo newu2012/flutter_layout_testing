23132ea131eca6114bc68755eb19c57d76c17bd1
newu2012 <newu2011@gmail.com>
2021-10-04 04:33:45 +0500

    feat: added buttons and selectable text to Camp

--- lib/Camp/Presentation/camp_widget.dart
+++ lib/Camp/Presentation/camp_widget.dart
@@ -24,12 +24,12 @@ class CampWidget extends StatelessWidget {
               children: [
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
+                  child: const SelectableText(
                     'Oeschinen Lake Campground',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
-                Text(
+                SelectableText(
                   'Kandersteg, Switzerland',
                   style: TextStyle(color: Colors.grey[500]),
                 ),
@@ -56,14 +56,13 @@ class CampWidget extends StatelessWidget {
 
     Widget textSection = const Padding(
       padding: EdgeInsets.all(32),
-      child: Text(
+      child: SelectableText(
         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
         'Alps. Situated 1,578 meters above sea level, it is one of the '
         'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
         'half-hour walk through pastures and pine forest, leads you to the '
         'lake, which warms to 20 degrees Celsius in the summer. Activities '
         'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
       ),
     );
 
@@ -77,24 +76,27 @@ class CampWidget extends StatelessWidget {
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+  TextButton _buildButtonColumn(Color color, IconData icon, String label) {
+    return TextButton(
+      onPressed: () {},
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
